97820a8d166accdf3f3a5516e80cbb8ca3407e1b
Anusha <nallagangulaanusha3@gmail.com>
2025-08-29 22:21:04 +0530

    ANUSHAN0001 - Wrote the code for checking whether a given number is even or odd

---
+++ src/main/java/com/example/javamavenjunithelloworld/Utility.js
@@ -0,0 +1,12 @@
+function checkEvenOrOdd(num){
+    if(num%2==0){
+        console.log(num+"Num is");
+    }
+    else{
+        console.log(num+"is Odd");
+    }
+}
+
+function Anusha(){
+    console.log("I am Anusha");
+}
